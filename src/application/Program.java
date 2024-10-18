e4267b65328e38adb988dff47832c3689e741bf4
Germano Manzoni <germano12344@gmail.com>
2024-10-18 14:51:57 -0300

    layer tabuleiro e print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
 
-        
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
     }
 }
